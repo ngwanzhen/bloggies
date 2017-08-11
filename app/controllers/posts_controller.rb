50db436828f42f367f60e9f5e14d170ebcf31837
Wanzhen Ng <ngwanzhen@gmail.com>
2017-08-11 11:29:00 +0800

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!,
+                only: [:new, :edit]
+# this means that if you don't login, you cannot see new and edit. but u can go to index, show
+  def index
+    news_url = 'https://newsapi.org/v1/articles?source=breitbart-news&sortBy=top&apiKey=51ff5e30d47f4063a25ca7f3c3cba6a8'
+    response = HTTParty.get(news_url)
+    @news_data = response
+
+    q = 'thunderstorms'
+    photo_url=
+    "https://pixabay.com/api/?key=6144418-f9b2e4313d5eeb7cd0f166436&q=#{q}&image_type=photo"
+    response = HTTParty.get(photo_url)
+    @photodata = response
+
+    @allposts = current_user.posts
+    @new_post = Post.new
+  end
+
+  def show
+    # render json: params
+    @currentpost = Post.find(params[:id])
+  end
+
+  def create
+    # render json: params
+    Post.create(
+    title: params[:post][:title],
+    content: params[:post][:content],
+    user_id: current_user.id
+    )
+    # creating_post = params.require(:post).permit(:title, :content, :user_id)
+    # Post.create(creating_post)
+    redirect_to '/posts/index'
+  end
+
+  def edit
+    @currentpost = Post.find(params[:id])
+  end
+
+  def destroy
+    Post.destroy(params[:id])
+    redirect_to posts_path
+  end
+
+  def update
+    currentpost = Post.find(params[:id])
+    updating_post = params.require(:post).permit(:title, :content)
+    redirect_to posts_path if currentpost.update(updating_post)
+    # toupdate = Post.find(params[:id])
+    # toupdate.title = params[:post][:title]
+    # toupdate.content = params[:post][:content]
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+
 root 'static#home'
+
+get '/about', to: 'static#about'
+
 devise_for :users,
 path: '',
 path_names: {
@@ -8,5 +15,7 @@ path_names: {
   sign_out: 'logout',
   sign_up: 'register'
 }
+
+resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
